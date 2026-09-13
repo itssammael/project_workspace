bff62d6aba02badcb48646627cdf1ac2a7a6afdb
Alamein Tabunda <prohackerz609@gmail.com>
2026-09-13 23:39:48 +0800

    fix: update login portal text to LGUNET Portal

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -50,7 +50,7 @@ const submit = () => {
                 <svg class="size-5 text-white shrink-0" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
                 </svg>
-                <span>Login with Login Portal</span>
+                <span>Login with LGUNET Portal</span>
             </a>
 
             <div class="relative my-5">
